94a942fc74fc7d2593fe3cf5df8fb2a4df86623c
Breno <breno.gomes@accountfy.com>
2021-07-13 14:00:00 -0300

    finish Chapter 4

--- TestCafeDemo/tests/skipTest.js
+++ TestCafeDemo/tests/skipTest.js
@@ -1,6 +1,15 @@
 fixture("First Fixture")
     .page("http://www.google.com/");
 
+test.skip.page("https://devexpress.github.io/testcafe/example/") 
+("First Test", async t => {
+    await t
+        .typeText("#developer-name","TAU")
+        .click("#macos")
+        .click("#submit-button")
+});
+
+
 test.page("https://devexpress.github.io/testcafe/example/") 
 ("First Test", async t => {
     await t

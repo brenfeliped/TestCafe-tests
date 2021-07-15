b5b596bfb8fc9c2b33d9a57bb80e6901a4554591
Breno <breno.gomes@accountfy.com>
2021-07-15 11:06:05 -0300

    Chapter 8 Data Driven Tests

--- TestCafeDemo/test/RegistrationTest.js
+++ TestCafeDemo/test/RegistrationTest.js
@@ -4,7 +4,7 @@ import RegisterPage from "../pages/RegisterPage";
 import LoginPage from "../pages/LoginPage";
 import CustomerPage from "../pages/CustomerPage";
 
-
+const dataSet = require('../data/data.json')
 const URL = "https://demo.nopcommerce.com/";
 const getURL = ClientFunction(() => window.location.href);
 var randomNumber = Math.floor(Math.random() * 50);
@@ -19,23 +19,23 @@ test('Assert Homes Page Test', async t => {
         .expect(HomePage.subtitleHeader.exists).ok();
 
 })
-
+dataSet.forEach(data =>{
 test('User Registration and Login Test', async t => {
     await t
     .click(HomePage.RegistersLink)
     .expect(getURL()).contains('register')
     .click(RegisterPage.GenderOption)
-    .typeText(RegisterPage.FirstName,"Bruce")
-    .typeText(RegisterPage.LastName,"Lee");
+    .typeText(RegisterPage.FirstName,data.firstname)
+    .typeText(RegisterPage.LastName,data.lastname);
 
-    await RegisterPage.selectDay('5');
-    await RegisterPage.selectMonth('November');
-    await RegisterPage.selectYear('1980');
+    await RegisterPage.selectDay(data.birthday);
+    await RegisterPage.selectMonth(data.birthmonth);
+    await RegisterPage.selectYear(data.birthyear);
     await t
-        .typeText(RegisterPage.Email, userEmail)
-        .typeText(RegisterPage.Password, '123456')
-        .typeText(RegisterPage.ConfirmPassword, '123456')
+        .typeText(RegisterPage.Email, data.email + randomNumber + "@test.com")
+        .typeText(RegisterPage.Password, data.password)
+        .typeText(RegisterPage.ConfirmPassword, data.password)
         .click(RegisterPage.RegisterButton)
         .expect(RegisterPage.SucessfulMessage.exists).ok()
         
-})
+}) });
